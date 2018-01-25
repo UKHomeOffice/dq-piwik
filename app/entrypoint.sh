9a2cd2d811a8c7a4e56237c405bd5c9127aa56f5
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-01-25 13:09:19 +0000

    wrap the entrypoint rather than replace it

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 set -e
-
-if [ ! -e piwik.php ]; then
-	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-	chown -R www-data .
-fi
+/entrypoint.sh
 
 sed -i -e "s/{PIWIK_MYSQL_HOST}/$PIWIK_MYSQL_HOST/" /var/www/html/config/config.ini.php
 sed -i -e "s/{PIWIK_MYSQL_USER}/$PIWIK_MYSQL_USER/" /var/www/html/config/config.ini.php
