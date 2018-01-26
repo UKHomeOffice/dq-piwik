a45b2d965bce12d47afd07ee4caa93f7e83b0b1c
Hemant <hemant.goswami@digital.homeoffice.gov.uk>
2018-01-26 15:06:18 +0000

    Updates mysql port

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 /entrypoint.sh
 
-sed -i -e "s/{PIWIK_MYSQL_HOST}/$PIWIK_MYSQL_HOST/" /var/www/html/config/config.ini.php
+sed -i -e "s/{PIWIK_MYSQL_HOST}/$PIWIK_MYSQL_HOST:$PIWIK_MYSQL_PORT/" /var/www/html/config/config.ini.php
 sed -i -e "s/{PIWIK_MYSQL_USER}/$PIWIK_MYSQL_USER/" /var/www/html/config/config.ini.php
 sed -i -e "s/{PIWIK_MYSQL_PASSWORD}/$PIWIK_MYSQL_PASSWORD/" /var/www/html/config/config.ini.php
 sed -i -e "s/{PIWIK_MYSQL_DBNAME}/$PIWIK_MYSQL_DBNAME/" /var/www/html/config/config.ini.php
